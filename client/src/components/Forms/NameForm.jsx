5465e80ba6b3775bc250100ddeac7d0279f9e11d
26june <gj.abordo1@gmail.com>
2023-11-14 10:09:21 +0000

    add materials ui and name text field

--- client/src/components/Forms/NameForm.jsx
+++ client/src/components/Forms/NameForm.jsx
@@ -1,16 +1,31 @@
 import React from "react";
+import TextField from "@mui/material/TextField";
 
 export default function NameForm({ nextStep }) {
   function handleSubmit(event) {
     event.preventDefault();
+    // event.target[0].value //Set state to this
     nextStep();
   }
 
   return (
     <div className="formContainer">
       <form className="centreForm" onSubmit={handleSubmit} id="nameForm">
-        <label htmlFor="cvInput">Name</label>
-        <input name="cvName" type="text" id="cvInput" />
+        <TextField
+          autoComplete="off"
+          spellCheck="false"
+          id="outlined-basic"
+          label="Name"
+          variant="standard"
+          InputProps={{ sx: { fontSize: "7vh", height: "10vh" } }}
+          InputLabelProps={{
+            sx: {
+              fontSize: "7vh",
+              "&.MuiInputLabel-shrink": { top: -50 },
+            },
+          }}
+          sx={{ width: "50%" }}
+        />
       </form>
       <input
         className="nextButton"
