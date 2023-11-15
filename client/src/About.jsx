bfa7af4fd94af76f620e0c3641fdde61a8ca688e
git <richard.james.grove@gmail.com>
2023-11-15 12:11:08 +0000

    newday

--- client/src/About.jsx
+++ client/src/About.jsx
@@ -4,10 +4,8 @@ export default function About() {
       <div>About</div>
 
       <p>Babtunde</p>
-      <img
-        src="https://models.readyplayer.me/6553834d9b792809cd99126b.glb"
-        alt="React Image"
-      />
+      <img src="/images/ReadyPlayerMe-Avatar(8).jpeg" alt="react image" />
+
       <p>
         I am a reliable team player, I bring exceptional resourcefulness and a
         knack for creative problem-solving to the table. Tech Educators Rock!!!
@@ -15,10 +13,7 @@ export default function About() {
       <a href="https://github.com/tundealfred">Git hub</a>
 
       <p>GJ</p>
-      <img
-        src="https://models.readyplayer.me/655378829b792809cd98ea4d.glb"
-        alt="React Image"
-      />
+      <img src="/images/ReadyPlayerMe-Avatar(3).jpeg" alt="React Image" />
       <p>
         Wannabe Software Developer Student who eventually wants to become a
         master at coding. Loves fixing bugs in code. Hates bugs IRL
@@ -26,10 +21,7 @@ export default function About() {
       <a href="https://github.com/26june">Git hub</a>
 
       <p>Neil</p>
-      <img
-        src="https://models.readyplayer.me/6553788af1a79ed2ebbd2941.glb"
-        alt="React Image"
-      />
+      <img src="/images/ReadyPlayerMe-Avatar(9).jpeg" alt="React Image" />
       <p>
         Getting back into coding after being busy with other things for a few
         years. Likes working. dislikes applying for work. If we can't improve
@@ -38,13 +30,10 @@ export default function About() {
       <a href="https://github.com/ned2070">Git hub</a>
 
       <p>Richard</p>
-      <img
-        src="https://models.readyplayer.me/6553660bf1a79ed2ebbcf149.glb"
-        alt="React Image"
-      />
+      <img src="/images/ReadyPlayerMe-Avatar(2).jpeg" alt="React Image" />
       <p>
         I am a good communicator and team player struggle with things but find a
-        way aroiund them to help the team out.
+        way around them to help the team out.
       </p>
       <a href="https://github.com/Ritchie2710">Git hub</a>
     </>
