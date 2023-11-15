344db1a6539df329fa1f0b597a954b62b5cbe841
git <richard.james.grove@gmail.com>
2023-11-15 15:39:35 +0000

    done

--- client/src/About.jsx
+++ client/src/About.jsx
@@ -3,63 +3,65 @@ export default function About() {
     <>
       <div>About</div>
 
-      <figure>
-        <h3>Babtunde</h3>
-        <img
-          className="about-image"
-          src="/images/ReadyPlayerMe-Avatar(8).jpeg"
-          alt="react image"
-        />
-        <figcaption>
-          I am a reliable team player, I bring exceptional resourcefulness and a
-          knack for creative problem-solving to the table. Tech Educators
-          Rock!!!
-        </figcaption>
-        <a href="https://github.com/tundealfred">Git hub</a>
-      </figure>
+      <div className="picdisplay">
+        <figure>
+          <h3>Babtunde</h3>
+          <img
+            className="about-image"
+            src="/images/ReadyPlayerMe-Avatar(8).jpeg"
+            alt="react image"
+          />
+          <figcaption>
+            I am a reliable team player, I bring exceptional resourcefulness and
+            a knack for creative problem-solving to the table. Tech Educators
+            Rock!!!
+          </figcaption>
+          <a href="https://github.com/tundealfred">Git hub</a>
+        </figure>
 
-      <figure>
-        <h3>GJ</h3>
-        <img
-          className="about-image"
-          src="/images/ReadyPlayerMe-Avatar(3).jpeg"
-          alt="React Image"
-        />
-        <figcaption>
-          Wannabe Software Developer Student who eventually wants to become a
-          master at coding. Loves fixing bugs in code. Hates bugs IRL
-        </figcaption>
-        <a href="https://github.com/26june">Git hub</a>
-      </figure>
+        <figure>
+          <h3>GJ</h3>
+          <img
+            className="about-image"
+            src="/images/ReadyPlayerMe-Avatar(3).jpeg"
+            alt="React Image"
+          />
+          <figcaption>
+            Wannabe Software Developer Student who eventually wants to become a
+            master at coding. Loves fixing bugs in code. Hates bugs IRL
+          </figcaption>
+          <a href="https://github.com/26june">Git hub</a>
+        </figure>
 
-      <figure>
-        <h3>Neil</h3>
-        <img
-          className="about-image"
-          src="/images/ReadyPlayerMe-Avatar(9).jpeg"
-          alt="React Image"
-        />
-        <figcaption>
-          Getting back into coding after being busy with other things for a few
-          years. Likes working. dislikes applying for work. If we can't improve
-          our own lives with code whose can we?
-        </figcaption>
-        <a href="https://github.com/ned2070">Git hub</a>
-      </figure>
+        <figure>
+          <h3>Neil</h3>
+          <img
+            className="about-image"
+            src="/images/ReadyPlayerMe-Avatar(9).jpeg"
+            alt="React Image"
+          />
+          <figcaption>
+            Getting back into coding after being busy with other things for a
+            few years. Likes working. dislikes applying for work. If we can't
+            improve our own lives with code whose can we?
+          </figcaption>
+          <a href="https://github.com/ned2070">Git hub</a>
+        </figure>
 
-      <figure>
-        <h3>Richard</h3>
-        <img
-          className="about-image"
-          src="/images/ReadyPlayerMe-Avatar(2).jpeg"
-          alt="React Image"
-        />
-        <figcaption>
-          I am a good communicator and team player struggle with things but find
-          a way around them to help the team out.
-        </figcaption>
-        <a href="https://github.com/Ritchie2710">Git hub</a>
-      </figure>
+        <figure>
+          <h3>Richard</h3>
+          <img
+            className="about-image"
+            src="/images/ReadyPlayerMe-Avatar(2).jpeg"
+            alt="React Image"
+          />
+          <figcaption>
+            I am a good communicator and team player struggle with things but
+            find a way around them to help the team out.
+          </figcaption>
+          <a href="https://github.com/Ritchie2710">Git hub</a>
+        </figure>
+      </div>
     </>
   );
 }
