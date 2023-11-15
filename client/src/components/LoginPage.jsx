48460533291f0243a378793659c1cf849b6f383e
git <richard.james.grove@gmail.com>
2023-11-15 15:26:20 +0000

    newadd

--- client/src/components/LoginPage.jsx
+++ client/src/components/LoginPage.jsx
@@ -5,13 +5,14 @@ function LoginPage() {
 
   return (
     <div className="LoginPage">
+      <img src="/images/CVlogo.png" alt="forge logo" />
       <h1> Welcome to CVFORGE.COM </h1>
       <p>
         where you can design your own CV ready to apply for jobs. You will enter
         some details of your education, experience in and out of the work place
-        and some personal details about you. To fill in your profile. at the end
-        of the form you will this website will generate a choice of templates
-        for you to choose from for you CV.
+        and some personal details about youself to fill in your profile. At the
+        end of the form you will this website will generate a choice of
+        templates for you to choose from for you CV.
       </p>
       <h2>Login to Start Building your CV!</h2>
 
