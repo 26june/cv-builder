9a41ad85a87aa1d1a55657a756826db759d13eef
ned2070te <ned2070te@gmail.com>
2023-11-15 14:30:55 +0000

    connecting to chatgpt

--- server/server.js
+++ server/server.js
@@ -9,6 +9,11 @@ app.use(cors());
 const axios = require("axios");
 mongoose.connect(process.env.DATABASE_URL);
 
+// For OpanAI API
+const OpenAI = require("openai");
+const openai = new OpenAI();
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
 app.get("/", (_, response) => response.json("Root route for cv-builder."));
 
 app.get("/cvs", async (request, response) => {
@@ -30,9 +35,32 @@ app.get("/cvs", async (request, response) => {
 
 // // DELETE CV from database
 // app.delete("/DATABASE_NAME/:id", async (request, response) => {
-//   const deletedCV = await Movie.findByIdAndDelete(request.params.id);
+//   const deletedCV = await CV.findByIdAndDelete(request.params.id);
 //   response.json(deletedCV);
 // });
+
+// Query ChatGPT for job advice
+const advice = async (req, res) => {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: "You are a helpful assistant",
+        role: "user",
+        content:
+          "List jobs I could do with these skills: data entry. Answer as a json array with the structure ['string']",
+      },
+    ],
+    model: "gpt-3.5-turbo-1106",
+    response_format: { "type": "json_object" },
+  });
+
+  const test = completion.choices[0].message.content;
+  console.log(test);
+  console.log(test.jobs);
+};
+// test advice function
+advice();
 
 // Confirm port active
 app.listen(PORT, () => console.log(`App is running PORT ${PORT}`));
